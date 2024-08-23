d4c9db681dffa0a93e98e92a7b9ea09c76b563bb
Daniel Beránek <daniel_beranek@icloud.com>
2024-08-23 17:39:08 +0200

    init Context

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,12 @@
 import { NextUIProvider } from '@nextui-org/react';
 import { type ReactNode } from 'react';
+import ContextProvider from '@/Context';
 
-export default function AppProviders({ children }: Readonly<{ children: ReactNode }>) {
-	return <NextUIProvider>{children}</NextUIProvider>;
-}
+const AppProviders = ({ children }: Readonly<{ children: ReactNode }>) => {
+	return (
+		<ContextProvider>
+			<NextUIProvider>{children}</NextUIProvider>
+		</ContextProvider>
+	);
+};
+export default AppProviders;
