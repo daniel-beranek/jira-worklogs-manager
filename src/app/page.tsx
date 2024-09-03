cf836e5e266d3bced143024ca559513213d43d85
Daniel Beránek <daniel_beranek@icloud.com>
2024-09-03 20:19:06 +0200

    add header, move worklogs and configuration to separate routes

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
+
+// Currently only redirects to worklogs or configuration pages
+const AppPage = () => {
+	const token = cookies().get('token');
+	const url = cookies().get('url');
+	const user = cookies().get('user');
+	if (typeof url === 'undefined' || typeof user === 'undefined' || typeof token === 'undefined') redirect('/configuration');
+	redirect('/worklogs');
+};
+export default AppPage;
