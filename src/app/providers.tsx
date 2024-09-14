b7a70378a459589d2549f8df1e9352e2218a93f9
Daniel Beránek <daniel_beranek@icloud.com>
2024-09-14 23:41:20 +0200

    add i18n provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,14 @@
+'use client';
+
 import { NextUIProvider } from '@nextui-org/react';
 import { type ReactNode } from 'react';
+import { I18nProvider } from '@react-aria/i18n';
 
 const AppProviders = ({ children }: Readonly<{ children: ReactNode }>) => {
-	return <NextUIProvider>{children}</NextUIProvider>;
+	return (
+		<I18nProvider>
+			<NextUIProvider>{children}</NextUIProvider>
+		</I18nProvider>
+	);
 };
 export default AppProviders;
