b8961e2753141e5a89f0d63b624f10120119fec3
Daniel Beránek <daniel_beranek@icloud.com>
2024-09-14 23:41:46 +0200

    remove top level error console.log

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,10 +3,6 @@ import { useEffect } from 'react';
 import { Button, CardBody, Card, CardHeader } from '@nextui-org/react';
 
 const AppError = ({ error, reset }: Readonly<{ error: Error & { digest?: string }; reset: () => void }>) => {
-	useEffect(() => {
-		console.error(error);
-	}, [error]);
-
 	return (
 		<Card className="mx-auto w-fit">
 			<CardBody className="flex flex-row items-center justify-center gap-3">
