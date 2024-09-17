9116361354971de3f8c8dc572d0fa75948cd4471
Daniel Beránek <daniel_beranek@icloud.com>
2024-09-18 00:07:45 +0200

    format errors with jsx

---
+++ src/app/worklogs/_actions/logWork.tsx
@@ -0,0 +1,59 @@
+'use server';
+import { getMultipleDecryptedCookies } from '@/lib/actions';
+import { Action, JiraData } from '@/types/types';
+
+export type LoggedWork = {
+	date: string;
+	timeSpentSeconds: number;
+};
+
+export const logWork: Action<LoggedWork, 'issueKeyOrId' | 'date' | 'timeSpentSeconds'> = async ({
+	issueKeyOrId,
+	date,
+	timeSpentSeconds
+}) => {
+	try {
+		const cookieRes = await getMultipleDecryptedCookies('url', 'token');
+		if (cookieRes.status !== 'success') return cookieRes;
+		const { url, token } = cookieRes.data;
+
+		const isoString = new Date(date).toISOString();
+		const dateString = isoString.slice(0, 10);
+		const jiraStarted = `${dateString}T12:00:00.000+0200`;
+
+		const res = await fetch(`https://${url}/rest/api/2/issue/${issueKeyOrId}/worklog`, {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${token}`,
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				timeSpentSeconds,
+				started: jiraStarted
+			})
+		});
+		const json: JiraData<'addWorklog', 201> = await res.json();
+		if (!res.ok) return { status: 'error', errors: json.errorMessages ?? ['Something went wrong'] };
+		if (typeof json.started === 'undefined' || typeof json.timeSpentSeconds === 'undefined')
+			return {
+				status: 'error',
+				errors: [
+					<p key={'missing response data'}>
+						Work logged successfully but was unable to get details back, please{' '}
+						<strong>reload the page</strong>
+					</p>
+				]
+			};
+
+		return {
+			status: 'success',
+			data: {
+				timeSpentSeconds: json.timeSpentSeconds,
+				date: new Date(json.started).toISOString()
+			}
+		};
+	} catch (e) {
+		console.error(e);
+		return { status: 'error', errors: ['Something went wrong'] };
+	}
+};
